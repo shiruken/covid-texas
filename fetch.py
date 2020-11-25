1c1a67e2d301758bf44301ed39c56b19fa10a97b
Colin Sullender <shiruken343@gmail.com>
2020-11-24 21:39:47 -0500

    Added daily case and death chart

--- fetch.py
+++ fetch.py
@@ -23,6 +23,56 @@ def fix_date_index(df):
     df.index.set_levels(date_index_fixed, level=2, inplace=True)
 
 
+tsa_county_map = {
+    'A.': ['Armstrong', 'Briscoe', 'Carson', 'Childress', 'Collingsworth', 'Dallam', 'Deaf Smith', 
+          'Donley', 'Gray', 'Hall', 'Hansford', 'Hartley', 'Hemphill', 'Hutchinson', 'Lipscomb', 
+          'Moore', 'Ochiltree', 'Oldham', 'Parmer', 'Potter', 'Randall', 'Roberts', 'Sherman', 
+          'Swisher', 'Wheeler'],
+    'B.': ['Bailey', 'Borden', 'Castro', 'Cochran', 'Cottle', 'Crosby', 'Dawson', 'Dickens', 'Floyd', 
+          'Gaines', 'Garza', 'Hale', 'Hockley', 'Kent', 'King', 'Lamb', 'Lubbock', 'Lynn', 'Motley', 
+          'Scurry', 'Terry', 'Yoakum'],
+    'C.': ['Archer', 'Baylor', 'Clay', 'Foard', 'Hardeman', 'Jack', 'Montague', 'Wichita', 'Wilbarger', 'Young'],
+    'D.': ['Brown', 'Callahan', 'Coleman', 'Comanche', 'Eastland', 'Fisher', 'Haskell', 'Jones', 
+          'Knox', 'Mitchell', 'Nolan', 'Shackelford', 'Stephens', 'Stonewall', 'Taylor', 'Throckmorton'],
+    'E.': ['Collin', 'Cooke', 'Dallas', 'Denton', 'Ellis', 'Erath', 'Fannin', 'Grayson', 'Hood', 
+          'Hunt', 'Johnson', 'Kaufman', 'Navarro', 'Palo Pinto', 'Parker', 'Rockwall', 
+          'Somervell', 'Tarrant', 'Wise'],
+    'F.': ['Bowie', 'Cass', 'Delta', 'Hopkins', 'Lamar', 'Morris', 'Red River', 'Titus'],
+    'G.': ['Anderson', 'Camp', 'Cherokee', 'Franklin', 'Freestone', 'Gregg', 'Harrison', 'Henderson', 
+          'Houston', 'Marion', 'Panola', 'Rains', 'Rusk', 'Shelby', 'Smith', 'Trinity', 'Upshur', 
+          'Van Zandt', 'Wood'],
+    'H.': ['Angelina', 'Nacogdoches', 'Polk', 'Sabine', 'San Augustine', 'San Jacinto', 'Tyler'],
+    'I.': ['Culberson', 'El Paso', 'Hudspeth'],
+    'J.': ['Andrews', 'Brewster', 'Crane', 'Ector', 'Glasscock', 'Howard', 'Jeff Davis', 'Loving', 
+          'Martin', 'Midland', 'Pecos', 'Presidio', 'Reeves', 'Terrell', 'Upton', 'Ward', 'Winkler'],
+    'K.': ['Coke', 'Concho', 'Crockett', 'Irion', 'Kimble', 'Mason', 'Mcculloch', 'Menard', 'Reagan', 
+          'Runnels', 'Schleicher', 'Sterling', 'Sutton', 'Tom Green'],
+    'L.': ['Bell', 'Coryell', 'Hamilton', 'Lampasas', 'Milam', 'Mills'],
+    'M.': ['Bosque', 'Falls', 'Hill', 'Limestone', 'Mclennan'],
+    'N.': ['Brazos', 'Burleson', 'Grimes', 'Leon', 'Madison', 'Robertson', 'Washington'],
+    'O.': ['Bastrop', 'Blanco' ,'Burnet' ,'Caldwell', 'Fayette', 'Hays', 'Lee', 'Llano', 'San Saba', 
+          'Travis', 'Williamson'],
+    'P.': ['Atascosa', 'Bandera', 'Bexar', 'Comal', 'Dimmit', 'Edwards', 'Frio', 'Gillespie', 
+          'Gonzales', 'Guadalupe', 'Karnes', 'Kendall', 'Kerr', 'Kinney', 'La Salle', 'Maverick', 
+          'Medina', 'Real', 'Uvalde', 'Val Verde', 'Wilson', 'Zavala'],
+    'Q.': ['Austin', 'Colorado', 'Fort Bend', 'Harris', 'Matagorda', 'Montgomery', 'Walker', 'Waller', 'Wharton'],
+    'R.': ['Brazoria', 'Chambers', 'Galveston', 'Hardin', 'Jasper', 'Jefferson', 'Liberty', 'Newton', 'Orange'],
+    'S.': ['Calhoun', 'Dewitt', 'De Witt', 'Goliad', 'Jackson', 'Lavaca', 'Victoria'],
+    'T.': ['Jim Hogg', 'Webb', 'Zapata'],
+    'U.': ['Aransas', 'Bee', 'Brooks', 'Duval', 'Jim Wells', 'Kenedy', 'Kleberg', 'Live Oak', 
+          'Mcmullen', 'Nueces', 'Refugio', 'San Patricio'],
+    'V.': ['Cameron', 'Hidalgo', 'Starr', 'Willacy'],
+    'Total': ['Total']
+}
+
+
+def map_county_to_tsa(county):
+    """Map county name to TSA"""
+    for k,v in tsa_county_map.items():
+        if county.title() in v:
+            return(k)
+
+
 # Load combined hospital data
 url = 'https://www.dshs.state.tx.us/coronavirus/CombinedHospitalDataoverTimebyTSA.xlsx'
 df = pd.read_excel(url, sheet_name=None, header=2, index_col=[0, 1], nrows=23)
@@ -71,9 +121,61 @@ df_covid_icu_inpatients = pd.DataFrame(df['COVID-19 ICU'].stack())
 df_covid_icu_inpatients.rename(columns = {0: 'covid_icu_inpatients'}, inplace=True)
 fix_date_index(df_covid_icu_inpatients)
 
+# Load and parse total case count data
+url = 'https://www.dshs.state.tx.us/coronavirus/TexasCOVID19DailyCountyCaseCountData.xlsx'
+df = pd.read_excel(url, sheet_name='Cases by County', index_col=[0], header=2, nrows=255)
+df.columns = [pd.to_datetime(date[6:] + '-20') for date in df.columns]
+df = df.loc[:, df.columns > '2020-04-11']
+df.insert(0, 'TSA ID', [map_county_to_tsa(county) for county in df.index])
+df = df.groupby(['TSA ID']).sum()
+tsa_location_map = dict(zip(df_total_beds_available.index.get_level_values(0).drop_duplicates(), 
+                            df_total_beds_available.index.get_level_values(1).drop_duplicates()))
+df['TSA AREA'] = [tsa_location_map[tsa] for tsa in df.index]
+df.set_index('TSA AREA', append=True, inplace=True)
+df_cases = pd.DataFrame(df.stack())
+df_cases.rename(columns = {0: 'cases'}, inplace=True)
+
+# Load and parse daily case count data
+url = 'https://www.dshs.state.tx.us/coronavirus/TexasCOVID-19NewCasesOverTimebyCounty.xlsx'
+df = pd.read_excel(url, sheet_name='New Cases by County', index_col=[0], header=2, nrows=255)
+df.columns = [pd.to_datetime(date[-5:] + '-20') for date in df.columns]
+df = df.loc[:, df.columns > '2020-04-11']
+df.insert(0, 'TSA ID', [map_county_to_tsa(county) for county in df.index])
+df = df.groupby(['TSA ID']).sum()
+df.loc['Total'] = df.sum(axis=0) # Add total category
+df['TSA AREA'] = [tsa_location_map[tsa] for tsa in df.index]
+df.set_index('TSA AREA', append=True, inplace=True)
+df_cases_new = pd.DataFrame(df.stack())
+df_cases_new.rename(columns = {0: 'cases_new'}, inplace=True)
+
+# Load and parse total death data
+url = 'https://www.dshs.state.tx.us/coronavirus/TexasCOVID19DailyCountyFatalityCountData.xlsx'
+df = pd.read_excel(url, sheet_name='Fatalities by County', index_col=[0], header=2, nrows=256)
+df.columns = [pd.to_datetime(date[11:] + '-20') for date in df.columns]
+df_new = df.diff(axis=1) # Extract daily death count
+df.drop(index='UNKNOWN', inplace=True)
+df = df.loc[:, df.columns > '2020-04-11']
+df.insert(0, 'TSA ID', [map_county_to_tsa(county) for county in df.index])
+df = df.groupby(['TSA ID']).sum()
+df[df_cases.index.get_level_values(2)[-1]] = df.iloc[:, -1] # Replicate last column to match date extent of case data
+df['TSA AREA'] = [tsa_location_map[tsa] for tsa in df.index]
+df.set_index('TSA AREA', append=True, inplace=True)
+df_deaths = pd.DataFrame(df.stack())
+df_deaths.rename(columns = {0: 'deaths'}, inplace=True)
+
+# Parse daily death data
+df_new = df_new.loc[:, df_new.columns > '2020-04-11']
+df_new.insert(0, 'TSA ID', [map_county_to_tsa(county) for county in df_new.index])
+df_new = df_new.groupby(['TSA ID']).sum()
+df_new['TSA AREA'] = [tsa_location_map[tsa] for tsa in df_new.index]
+df_new.set_index('TSA AREA', append=True, inplace=True)
+df_deaths_new = pd.DataFrame(df_new.stack())
+df_deaths_new.rename(columns = {0: 'deaths_new'}, inplace=True)
+
 # Merge the datasets
 df = [df_total_beds_available, df_total_beds_occupied, df_icu_beds_available, 
-      df_icu_beds_occupied, df_covid_inpatients, df_covid_icu_inpatients]
+      df_icu_beds_occupied, df_covid_inpatients, df_covid_icu_inpatients, 
+      df_cases, df_cases_new, df_deaths, df_deaths_new]
 df_merged = pd.concat(df, join='outer', axis=1).reindex(df_total_beds_available.index)
 
 # Reset the index
@@ -87,9 +189,18 @@ df_merged.to_csv('data.csv', index=False, float_format='%d', date_format='%Y-%m-
 # Manual removals of extreme values
 df_merged.loc[(df_merged['tsa'] == 'N') & (df_merged['date'] == '2020-04-28'), 'total_beds_occupied'] = np.nan
 
+df_merged.loc[(df_merged['tsa'] == 'A') & (df_merged['date'] == '2020-05-16'), 'cases_new'] = np.nan
+
 df_merged.loc[(df_merged['tsa'] == 'I') & (df_merged['date'] == '2020-05-31'), 'icu_beds_occupied'] = np.nan
 
 df_merged.loc[(df_merged['tsa'] == 'C') & (df_merged['date'] == '2020-06-07'), ['total_beds_occupied', 'icu_beds_occupied']] = np.nan
+
+df_merged.loc[(df_merged['tsa'] == 'G') & (df_merged['date'] == '2020-06-16'), 'cases_new'] = np.nan
+
+df_merged.loc[(df_merged['tsa'] == 'K') & (df_merged['date'] == '2020-07-14'), 'cases_new'] = np.nan
+
+df_merged.loc[(df_merged['tsa'] == 'P') & (df_merged['date'] == '2020-07-17'), 'cases_new'] = np.nan
+df_merged.loc[(df_merged['tsa'] == 'Total') & (df_merged['date'] == '2020-07-17'), 'cases_new'] = np.nan
 
 df_merged.loc[(df_merged['tsa'] == 'Total') & (df_merged['date'] == '2020-07-23'), ['total_beds_occupied', 'icu_beds_occupied', 'covid_inpatients']] = np.nan
 df_merged.loc[(df_merged['tsa'] == 'B') & (df_merged['date'] == '2020-07-23'), ['total_beds_occupied', 'icu_beds_occupied']] = np.nan
@@ -151,6 +262,7 @@ df_merged.loc[(df_merged['tsa'] == 'H') & (df_merged['date'] == '2020-07-27'), '
 df_merged.loc[(df_merged['tsa'] == 'J') & (df_merged['date'] == '2020-07-27'), 'total_beds_available'] = np.nan
 df_merged.loc[(df_merged['tsa'] == 'K') & (df_merged['date'] == '2020-07-27'), 'covid_inpatients'] = np.nan
 df_merged.loc[(df_merged['tsa'] == 'M') & (df_merged['date'] == '2020-07-27'), ['total_beds_occupied', 'icu_beds_occupied']] = np.nan
+df_merged.loc[(df_merged['tsa'] == 'N') & (df_merged['date'] == '2020-07-27'), 'cases_new'] = np.nan
 df_merged.loc[(df_merged['tsa'] == 'R') & (df_merged['date'] == '2020-07-27'), 'total_beds_occupied'] = np.nan
 df_merged.loc[(df_merged['tsa'] == 'V') & (df_merged['date'] == '2020-07-27'), ['total_beds_occupied', 'total_beds_available', 'icu_beds_available']] = np.nan
 
@@ -169,13 +281,16 @@ df_merged.loc[(df_merged['tsa'] == 'R') & (df_merged['date'] == '2020-07-31'), '
 df_merged.loc[(df_merged['tsa'] == 'Q') & (df_merged['date'] == '2020-07-31'), 'total_beds_occupied'] = np.nan
 
 df_merged.loc[(df_merged['tsa'] == 'G') & (df_merged['date'] == '2020-08-01'), 'icu_beds_occupied'] = np.nan
+df_merged.loc[(df_merged['tsa'] == 'J') & (df_merged['date'] == '2020-08-01'), 'cases_new'] = np.nan
 df_merged.loc[(df_merged['tsa'] == 'K') & (df_merged['date'] == '2020-08-01'), 'icu_beds_occupied'] = np.nan
 df_merged.loc[(df_merged['tsa'] == 'M') & (df_merged['date'] == '2020-08-01'), 'icu_beds_occupied'] = np.nan
-df_merged.loc[(df_merged['tsa'] == 'U') & (df_merged['date'] == '2020-08-01'), 'icu_beds_occupied'] = np.nan
+df_merged.loc[(df_merged['tsa'] == 'U') & (df_merged['date'] == '2020-08-01'), ['icu_beds_occupied', 'cases_new']] = np.nan
 
 df_merged.loc[(df_merged['tsa'] == 'F') & (df_merged['date'] == '2020-08-02'), 'icu_beds_occupied'] = np.nan
 
 df_merged.loc[(df_merged['tsa'] == 'P') & (df_merged['date'] == '2020-09-16'), 'total_beds_occupied'] = np.nan
+
+df_merged.loc[(df_merged['tsa'] == 'A') & (df_merged['date'] == '2020-09-30'), 'cases_new'] = np.nan
 
 df_merged.loc[(df_merged['tsa'] == 'J') & (df_merged['date'] == '2020-10-09'), 'total_beds_occupied'] = np.nan
 
@@ -184,6 +299,11 @@ df_merged.loc[(df_merged['tsa'] == 'K') & (df_merged['date'] == '2020-10-20'), [
 df_merged.loc[(df_merged['tsa'] == 'Q') & (df_merged['date'] == '2020-11-01'), 'total_beds_occupied'] = np.nan
 
 df_merged.loc[(df_merged['tsa'] == 'S') & (df_merged['date'] == '2020-11-02'), 'icu_beds_available'] = np.nan
+
+df_merged.loc[(df_merged['tsa'] == 'D') & (df_merged['date'] == '2020-11-11'), 'cases_new'] = np.nan
+
+# Eliminate negative values from cases_new
+df_merged.loc[df_merged['cases_new'] < 0, 'cases_new'] = np.nan
 
 # Write cleaned data to CSV file
 df_merged.to_csv('docs/data.csv', index=False, float_format='%d', date_format='%Y-%m-%d')
