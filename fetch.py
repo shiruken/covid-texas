8c2664ef0431726340203fd2b22c6edcc7b0e207
Colin Sullender <shiruken343@gmail.com>
2021-07-11 17:48:59 -0400

    Update 2021-07-11

--- fetch.py
+++ fetch.py
@@ -343,6 +343,9 @@ df_merged.loc[(df_merged['tsa'] == 'C') & (df_merged['date'] == '2021-01-11'), '
 df_merged.loc[(df_merged['tsa'] == 'G') & (df_merged['date'] == '2021-01-11'), ['total_beds_occupied', 'icu_beds_occupied']] = np.nan
 df_merged.loc[(df_merged['tsa'] == 'J') & (df_merged['date'] == '2021-01-11'), 'total_beds_occupied'] = np.nan
 
+df_merged.loc[(df_merged['tsa'] == 'J') & (df_merged['date'] == '2021-06-29'), 'icu_beds_occupied'] = np.nan
+df_merged.loc[(df_merged['tsa'] == 'T') & (df_merged['date'] == '2021-06-29'), 'icu_beds_occupied'] = np.nan
+
 # Eliminate negative values from cases_new
 df_merged.loc[df_merged['cases_new'] < 0, 'cases_new'] = np.nan
 
